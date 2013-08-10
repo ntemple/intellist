dc8f3e60dc8ce6bc13eac3b8703c128ee66c3857
Nick Temple <nick@intellispire.com>
2013-08-09 23:59:32 -0700

    tests should be passing

--- Tests/ConfigTest.php
+++ Tests/ConfigTest.php
@@ -5,11 +5,11 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
     function testInstall() {
         $this->assertTrue(true);
     }
-
+/*
     function testFail() {
         $this->assertFalse(true);
     }
-
+*/
     function testMongoConnect() {
         $conn = get_db_connection(); 
         $this->assertNotNull($conn);
